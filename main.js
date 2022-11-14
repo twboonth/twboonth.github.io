9b6aeeeb6b4df6681c8190750968ef03db0d9484
twboonth <twboonthom@gmail.com>
2022-11-14 10:39:23 +0700

    adding style sheet , JavaScript
    Resume page

---
+++ main.js
@@ -0,0 +1,56 @@
+// Menu links
+let getAbout = document.getElementById("getAbout");
+let getResume = document.getElementById("getResume");
+let getContact = document.getElementById("getContact");
+let btnContact = document.getElementById("btnContact");
+
+
+// Sections
+let about = document.getElementById("about");
+let resume = document.getElementById("resume");
+let contact = document.getElementById("contact");
+
+function removeClass() {
+    // Links
+    getAbout.classList.remove('selected');
+    getResume.classList.remove('selected');
+    getContact.classList.remove('selected');
+    // Sections
+    about.classList.remove('view');
+    resume.classList.remove('view');
+    contact.classList.remove('view');
+}
+
+getAbout.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        about.classList.add('view');
+        getAbout.classList.add('selected');
+    }
+
+});
+getResume.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        resume.classList.add('view');
+        getResume.classList.add('selected');
+    }
+})
+getContact.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        contact.classList.add('view');
+        getContact.classList.add('selected');
+    }
+})
+btnContact.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        contact.classList.add('view');
+        getContact.classList.add('selected');
+    }
+})
